173587c81f80b37ce8cf60c09dedaa0b342e29c7
Fernedy González Arias <fernedy.arias@gmail.com>
2020-04-18 22:18:37 -0500

    Add Css and Js BlogFrontend

---
+++ resources/views/layouts/InstaHuntershow.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!---Meta-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="InstHunters" />
+    <meta name="author" content="" />
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('blogFrontend/assets/images/favicon.ico') }}">
+
+    <title>InstaHuter Preview</title>
+
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('blogFrontend/assets/js/jquery-1.11.3.min.js') }}" defer></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('blogFrontend/assets/css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ asset('blogFrontend/assets/css/font-icons/entypo/css/entypo.css') }}">
+	<link rel="stylesheet" href="{{ asset('blogFrontend/assets/css/neon.css') }}">
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                       INICIO
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+                            </li>
+                            <!-- @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>-->
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+	<!-- Bottom scripts (common) -->
+	<script src="{{ asset('blogFrontend/assets/js/gsap/TweenMax.min.js') }}" defer></script>
+	<script src="{{ asset('blogFrontend/assets/js/bootstrap.js') }}" defer></script>
+	<script src="{{ asset('blogFrontend/assets/js/joinable.js') }}" defer></script>
+	<script src="{{ asset('blogFrontend/assets/js/resizeable.js') }}" defer></script>
+
+
+	<!-- JavaScripts initializations and stuff -->
+	<script src="{{ asset('blogFrontend/assets/js/neon-custom.js') }}" defer></script>
+</body>
+</html>
